45e7a954b03e151ede68b073a1c25e7cc012d3b8
lijian1234 <www.591466539@qq.com>
2018-11-29 16:53:16 +0800

    test
    Change-Id: I57b90a522fc96165d9f104a21c04ac7df70e6adf

--- app/Http/Controllers/TemplateController.php
+++ app/Http/Controllers/TemplateController.php
@@ -2,84 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Template;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 
-class TemplateController extends Controller
+class TemplateController extends Controller 
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Template  $template
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Template $template)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Template  $template
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Template $template)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Template  $template
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Template $template)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Template  $template
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Template $template)
-    {
-        //
-    }
+	public function getTemplate() {
+		sleep(1);
+		$dataSource = Input::get('dataSource');
+		$dataType = Input::get('dataType');
+		$arr = array(
+				array('value'=>$dataSource, 'label'=>$dataSource),
+				array('value'=>$dataType, 'label'=>$dataType)
+					);
+		return $arr;
+	}
 }
