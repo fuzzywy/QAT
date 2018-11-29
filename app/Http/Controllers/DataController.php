5e2136aa4c46ab7fa0d46381367b7934dee4212a
lijian1234 <www.591466539@qq.com>
2018-11-29 15:21:48 +0800

    commit after rebase patch 2
    Change-Id: I14169d8e66da7c571ea1146fe150fddba2c80354

---
+++ app/Http/Controllers/DataController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+
+class DataController extends Controller 
+{
+	public function getQatData() {
+		sleep(1);
+		$dataSource = Input::get('dataSource');
+		$dataType = Input::get('dataType');
+		$arr = array(
+				array('date'=>'2016-05-03', 'name'=>'1', 'address'=>$dataSource, '这是萨达萨达是啊是大大撒大撒大撒撒打算的阿萨大啊'=>$dataSource ),
+				array('date'=>'2016-05-03', 'name'=>'2', 'address'=>$dataSource, '这是萨达萨达是啊是大大撒大撒大撒撒打算的阿萨大啊'=>$dataSource ),
+				array('date'=>'2016-05-03', 'name'=>'3', 'address'=>$dataSource, '这是萨达萨达是啊是大大撒大撒大撒撒打算的阿萨大啊'=>$dataSource ),
+				array('date'=>'2016-05-03', 'name'=>'4', 'address'=>$dataSource, '这是萨达萨达是啊是大大撒大撒大撒撒打算的阿萨大啊'=>$dataSource ),
+				array('date'=>'2016-05-03', 'name'=>'5', 'address'=>$dataSource, '这是萨达萨达是啊是大大撒大撒大撒撒打算的阿萨大啊'=>$dataSource ),
+				array('date'=>'2016-05-03', 'name'=>'6', 'address'=>$dataSource, '这是萨达萨达是啊是大大撒大撒大撒撒打算的阿萨大啊'=>$dataSource ),
+				array('date'=>'2016-05-03', 'name'=>'7', 'address'=>$dataSource, '这是萨达萨达是啊是大大撒大撒大撒撒打算的阿萨大啊'=>$dataSource ),
+				array('date'=>'2016-05-03', 'name'=>'8', 'address'=>$dataSource, '这是萨达萨达是啊是大大撒大撒大撒撒打算的阿萨大啊'=>$dataSource ),
+				array('date'=>'2016-05-03', 'name'=>'9', 'address'=>$dataSource, '这是萨达萨达是啊是大大撒大撒大撒撒打算的阿萨大啊'=>$dataSource ),
+				array('date'=>'2016-05-03', 'name'=>'王小虎', 'address'=>$dataSource, '这是萨达萨达是啊是大大撒大撒大撒撒打算的阿萨大啊'=>$dataSource ),
+				array('date'=>'2016-05-03', 'name'=>'王小虎', 'address'=>$dataSource, '这是萨达萨达是啊是大大撒大撒大撒撒打算的阿萨大啊'=>$dataSource ),
+				array('date'=>'2016-05-03', 'name'=>'王小虎', 'address'=>$dataSource, '这是萨达萨达是啊是大大撒大撒大撒撒打算的阿萨大啊'=>$dataSource ),
+				array('date'=>'2016-05-03', 'name'=>'王小虎', 'address'=>$dataSource, '这是萨达萨达是啊是大大撒大撒大撒撒打算的阿萨大啊'=>$dataSource ),
+				array('date'=>'2016-05-03', 'name'=>'王小虎', 'address'=>$dataSource, '这是萨达萨达是啊是大大撒大撒大撒撒打算的阿萨大啊'=>$dataSource ),
+				array('date'=>'2016-05-03', 'name'=>'王小虎', 'address'=>$dataSource, '这是萨达萨达是啊是大大撒大撒大撒撒打算的阿萨大啊'=>$dataSource ),
+				array('date'=>'2016-05-03', 'name'=>'王小虎', 'address'=>$dataSource, '这是萨达萨达是啊是大大撒大撒大撒撒打算的阿萨大啊'=>$dataSource ),
+				array('date'=>'2016-05-03', 'name'=>'王小虎', 'address'=>$dataSource, '这是萨达萨达是啊是大大撒大撒大撒撒打算的阿萨大啊'=>$dataSource ),
+				array('date'=>'2016-05-03', 'name'=>'王小虎', 'address'=>$dataType, '这是萨达萨达是啊是大大撒大撒大撒撒打算的阿萨大啊'=>$dataSource)
+					);
+		return $arr;
+	}
+}
---
+++ resources/assets/js/api/uploadCog.js
@@ -0,0 +1,115 @@
+/**
+ * Imports the uploadCog URL from the config.
+ */
+ export default {
+ 	uploadCog: function( ip, connname, city, port, database, user, pwd, type, subnetwork, subnetworkfdd, subnetworknbiot ) {
+ 		let postData = qs.stringify({
+ 			// inCharset: 'utf-8',
+ 			// outCharset: 'utf-8',
+		    ip: ip,
+		    connname: connname,
+		    city: city,
+			port: port,
+			database: database,
+			user: user,
+			pwd: pwd,
+			type: type,
+			subnetwork: subnetwork,
+			subnetworkfdd: subnetworkfdd,
+			subnetworknbiot: subnetworknbiot
+		});
+		/*eval(" var g_postData = '"+postData+"';");
+		console.log(g_postData)*/
+ 		return axios.post('uploadCog', {
+            data: postData
+        })
+        .catch(function(error) {
+			if (error.response) {
+		      	// 发送请求后，服务端返回的响应码不是 2xx   
+		      	// console.log(error.response.data);
+		      	// console.log(error.response.status);
+		      	// console.log(error.response.headers);
+		      	if ( error.response.status == '404' ) {
+		      		return error.response.status + ' Not Found';
+		      	}else {
+		      		return error.response.status;
+		      	}
+		    } else if (error.request) {
+			    // 发送请求但是没有响应返回
+			    return 'Request failed'
+			    // return error.request
+			    // console.log(error.request);
+		    } else {
+		      	// 其他错误
+		      	return 'Other errors'
+		      	// return error.message
+		      	// console.log('Error', error.message);
+		    }
+		    // console.log(error.config);
+		});
+ 	},
+ 	showCog: function() {
+ 		return axios.post('showCog')
+			.catch(function(error) {
+			if (error.response) {
+		      	// 发送请求后，服务端返回的响应码不是 2xx   
+		      	if ( error.response.status == '404' ) {
+		      		return error.response.status + ' Not Found';
+		      	}else {
+		      		return error.response.status;
+		      	}
+		    } else if (error.request) {
+			    // 发送请求但是没有响应返回
+			    return 'Request failed'
+		    } else {
+		      	// 其他错误
+		      	return 'Other errors'
+		    }
+		    // console.log(error.config);
+		});
+ 	},
+ 	deleteCog: function( ip, connname, city, port, database, user, pwd, type, subnetwork, subnetworkfdd, subnetworknbiot ) {
+ 		let postData = qs.stringify({
+ 			// inCharset: 'utf-8',
+ 			// outCharset: 'utf-8',
+		    ip: ip,
+		    connname: connname,
+		    city: city,
+			port: port,
+			database: database,
+			user: user,
+			pwd: pwd,
+			type: type,
+			subnetwork: subnetwork,
+			subnetworkfdd: subnetworkfdd,
+			subnetworknbiot: subnetworknbiot
+		});
+ 		return axios.post( 'deleteCog', {
+            data: postData
+        })
+        .catch(function(error) {
+			if (error.response) {
+		      	// 发送请求后，服务端返回的响应码不是 2xx   
+		      	// console.log(error.response.data);
+		      	// console.log(error.response.status);
+		      	// console.log(error.response.headers);
+		      	if ( error.response.status == '404' ) {
+		      		return error.response.status + ' Not Found';
+		      	}else {
+		      		return error.response.status;
+		      	}
+		    } else if (error.request) {
+			    // 发送请求但是没有响应返回
+			    return 'Request failed'
+			    // return error.request
+			    // console.log(error.request);
+		    } else {
+		      	// 其他错误
+		      	return 'Other errors'
+		      	// return error.message
+		      	// console.log('Error', error.message);
+		    }
+		    // console.log(error.config);
+		});
+ 	}
+ }
