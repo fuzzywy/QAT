6c6d4e4c009fa70955ce765409f9d99e372dc4db
lijian1234 <www.591466539@qq.com>
2018-11-29 15:16:11 +0800

    commit after rebase
    Change-Id: Iab0eec760a3117c07c179f29e387691ede36b492

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,42 @@
+window._ = require('lodash');
+try {
+    window.$ = window.jQuery = require('jquery');
+} catch (e) {}
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+window.axios = require('axios');
 
-require('./bootstrap');
+window.qs = require('qs');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
 
 window.Vue = require('vue');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI, { size: 'big', zIndex: 3000 });
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+import router from './routes.js';
+
+import store from './store.js';
+
+window.BootstrapVue  = require('bootstrap-vue');
+
+window.VueHighcharts = require('vue-highcharts');
+window.Highcharts = require('highcharts');
+
+//data transform
+const bus = new Vue()
+Vue.prototype.bus = bus
 
 const app = new Vue({
-    el: '#app'
-});
+  router,
+  store
+}).$mount('#app')
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,37 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return view('auth/login');
 });
+Auth::routes();
+
+Route::get('localeLang', 'NavController@localeLang');
+
+Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/cog', 'CogController@index')->name('cog');
+
+
+Route::get('getTabs', 'NetworkOverviewController@getTabs');
+Route::get('getScaleTabs', 'NetworkOverviewController@getScaleTabs');
+Route::get('getLoadTabs', 'NetworkOverviewController@getLoadTabs');
+Route::get('getcharts', 'NetworkOverviewController@getcharts');
+
+
+Route::get('test', 'TestController@test');
+
+Route::get('getCity', 'NetworkOverviewController@getCity');
+Route::get('getBirdSideBar', 'NetworkOverviewController@getBirdSideBar');
+
+Route::post('uploadCog', 'UploadController@uploadCog');
+Route::post('showCog', 'UploadController@showCog');
+Route::post('deleteCog', 'UploadController@deleteCog');
+
+// Route::group(['prefix' => 'qat'], function () {
+Route::get('getQatSubnet', 'SubnetController@getSubnet');
+Route::get('getQatMoban', 'MobanController@getMoban');
+Route::get('getQatCity', 'CityController@getCity');
+Route::get('getQatTime', 'TimeController@getTime');
+Route::get('getQatLocation', 'LocationController@getLocation');
+Route::get('getQatData', 'DataController@getQatData');
+// });
