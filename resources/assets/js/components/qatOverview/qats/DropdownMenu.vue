1ffef13f1224a4e61927e892d5c3a43ec3059bd6
lijian1234 <www.591466539@qq.com>
2018-12-04 15:06:02 +0800

    使用element重写dropdow menu
    Change-Id: I500000924e11246dddab4dd6d81cd12dc69db61f
    Signed-off-by: lijian1234 <www.591466539@qq.com>

--- resources/assets/js/components/qatOverview/qats/DropdownMenu.vue
+++ resources/assets/js/components/qatOverview/qats/DropdownMenu.vue
@@ -1,97 +1,138 @@
 <style>
-	.dropdown {
-		text-align: center;
-	}
+    .dropdown {
+        top: 5px;
+        text-align: center;
+    }
 </style>
 
 <template>
-	<div id="dropdown" class="dropdown">
-		<b-dropdown id="eniq" :text="eniqkpi" class="m-md-2">
-			<b-dropdown-header>KPI</b-dropdown-header>
-			<b-dropdown-divider></b-dropdown-divider>
-		    <b-dropdown-item @click="types('ENIQ', 'TDD')" to="eniqtdd">TDD</b-dropdown-item>
-		    <b-dropdown-item @click="types('ENIQ', 'FDD')" to="eniqfdd">FDD</b-dropdown-item>
-		    <b-dropdown-item @click="types('ENIQ', 'GSM')" to="eniqgsm">GSM</b-dropdown-item>
-		    <b-dropdown-item @click="types('ENIQ', 'NBIOT')" to="eniqnbiot">NBIOT</b-dropdown-item>
-		    <b-dropdown-item disabled @click="types('ALARM')" to="eniqalarm">ALARM</b-dropdown-item>
-		</b-dropdown>
-		<b-dropdown id="nbm" :text="nbmkpi" class="m-md-2">
-			<b-dropdown-header>KPI</b-dropdown-header>
-			<b-dropdown-divider></b-dropdown-divider>
-		    <b-dropdown-item @click="types('NBM', 'TDD')" to="nbmtdd">TDD</b-dropdown-item>
-		    <b-dropdown-item @click="types('NBM', 'FDD')" to="nbmfdd">FDD</b-dropdown-item>
-		    <b-dropdown-item @click="types('NBM', 'NBIOT')" to="nbmnbiot">NBIOT</b-dropdown-item>
-		</b-dropdown>
-		<b-dropdown id="kget" :text="kgetkpi" class="m-md-2">
-			<b-dropdown-header>KPI</b-dropdown-header>
-			<b-dropdown-divider></b-dropdown-divider>
-		    <b-dropdown-item @click="types('KGET', '')">KGET</b-dropdown-item>
-		</b-dropdown>
-		<b-dropdown id="mr" :text="mrkpi" class="m-md-2">
-			<b-dropdown-header>KPI</b-dropdown-header>
-			<b-dropdown-divider></b-dropdown-divider>
-		    <b-dropdown-item @click="types('MR', '')">MR</b-dropdown-item>
-		</b-dropdown>
-		<b-dropdown id="ctr" :text="ctrkpi" class="m-md-2">
-			<b-dropdown-header>KPI</b-dropdown-header>
-			<b-dropdown-divider></b-dropdown-divider>
-		    <b-dropdown-item @click="types('CTR', '')">CTR</b-dropdown-item>
-		</b-dropdown>
-		<b-dropdown id="ctum" :text="ctumkpi" class="m-md-2">
-			<b-dropdown-header>KPI</b-dropdown-header>
-			<b-dropdown-divider></b-dropdown-divider>
-		    <b-dropdown-item @click="types('CTUM', '')">CTUM</b-dropdown-item>
-		</b-dropdown>
-		<b-dropdown id="ebm" :text="ebmkpi" class="m-md-2">
-			<b-dropdown-header>KPI</b-dropdown-header>
-			<b-dropdown-divider></b-dropdown-divider>
-		    <b-dropdown-item @click="types('EBM', '')">EBM</b-dropdown-item>
-		</b-dropdown>
-	</div>
+    <div id="dropdown" class="dropdown">
+        <el-dropdown @command="handleCommand">
+            <el-button type="primary" size="medium">
+                {{eniqkpi}}<i class="el-icon-arrow-down el-icon--right"></i>
+            </el-button>
+            <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item disabled>KPI</el-dropdown-item>
+                <el-dropdown-item divided command="ENIQ-TDD">TDD</el-dropdown-item>
+                <el-dropdown-item command="ENIQ-FDD">FDD</el-dropdown-item>
+                <el-dropdown-item command="ENIQ-GSM">GSM</el-dropdown-item>
+                <el-dropdown-item command="ENIQ-NBIOT">NBIOT</el-dropdown-item>
+                <el-dropdown-item disabled command="ENIQ-ALARM">ALARM</el-dropdown-item>
+            </el-dropdown-menu>
+        </el-dropdown>
+        <el-dropdown @command="handleCommand">
+            <el-button type="primary" size="medium">
+                {{nbmkpi}}<i class="el-icon-arrow-down el-icon--right"></i>
+            </el-button>
+            <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item disabled>KPI</el-dropdown-item>
+                <el-dropdown-item divided command="NBM-TDD">TDD</el-dropdown-item>
+                <el-dropdown-item command="NBM-FDD">FDD</el-dropdown-item>
+                <el-dropdown-item command="NBM-NBIOT">NBIOT</el-dropdown-item>
+            </el-dropdown-menu>
+        </el-dropdown>
+        <el-dropdown @command="handleCommand">
+            <el-button type="primary" size="medium">
+                {{kgetkpi}}<i class="el-icon-arrow-down el-icon--right"></i>
+            </el-button>
+            <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item disabled>KPI</el-dropdown-item>
+                <el-dropdown-item divided command="KGET-">KGET</el-dropdown-item>
+            </el-dropdown-menu>
+        </el-dropdown>
+        <el-dropdown @command="handleCommand">
+            <el-button type="primary" size="medium">
+                {{mrkpi}}<i class="el-icon-arrow-down el-icon--right"></i>
+            </el-button>
+            <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item disabled>KPI</el-dropdown-item>
+                <el-dropdown-item divided command="MR-">MR</el-dropdown-item>
+            </el-dropdown-menu>
+        </el-dropdown>
+        <el-dropdown @command="handleCommand">
+            <el-button type="primary" size="medium">
+                {{ctrkpi}}<i class="el-icon-arrow-down el-icon--right"></i>
+            </el-button>
+            <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item disabled>KPI</el-dropdown-item>
+                <el-dropdown-item divided command="CTR-">CTR</el-dropdown-item>
+            </el-dropdown-menu>
+        </el-dropdown>
+        <el-dropdown @command="handleCommand">
+            <el-button type="primary" size="medium">
+                {{ctumkpi}}<i class="el-icon-arrow-down el-icon--right"></i>
+            </el-button>
+            <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item disabled>KPI</el-dropdown-item>
+                <el-dropdown-item divided command="CTUM-">CTUM</el-dropdown-item>
+            </el-dropdown-menu>
+        </el-dropdown>
+        <el-dropdown @command="handleCommand">
+            <el-button type="primary" size="medium">
+                {{ebmkpi}}<i class="el-icon-arrow-down el-icon--right"></i>
+            </el-button>
+            <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item disabled>KPI</el-dropdown-item>
+                <el-dropdown-item divided command="EBM-">EBM</el-dropdown-item>
+            </el-dropdown-menu>
+        </el-dropdown>
+    </div>
 </template>
-
 <script>
-	export default {
-		data() {
-			return {
-				eniqkpi: "ENIQ",
-				nbmkpi: "NBM",
-				kgetkpi: "KGET",
-				mrkpi: "MR",
-				ctrkpi: "CTR",
-				ctumkpi: "CTUM",
-				ebmkpi: "EBM"
-			}
-		},
-		watch: {
-
-		},
-		methods: {
-			types: function(ft, st) {
-				this.eniqkpi = 'ENIQ';
-				this.nbmkpi = 'NBM';
-				this.kgetkpi = 'KGET';
-				this.mrkpi = "MR";
-				this.ctrkpi = "CTR";
-				this.ctumkpi = "CTUM";
-				this.ebmkpi = "EBM";
-				if ( ft == 'ENIQ' ) {
-					this.eniqkpi = 'ENIQ - ' + st;
-				} else if( ft == 'NBM' ) {
-					this.nbmkpi = 'NBM - ' + st;
-				} else if( ft == 'KGET' ) {
-					this.kgetkpi = 'KGET - ' + st;
-				}  else if( ft == 'MR' ) {
-					this.mrkpi = 'MR - ' + st;
-				} else if( ft == 'CTR' ) {
-					this.ctrkpi = 'CTR - ' + st;
-				} else if( ft == 'CTUM' ) {
-					this.ctumkpi = 'CTUM - ' + st;
-				} else if( ft == 'EBM' ) {
-					this.ebmkpi = 'EBM - ' + st;
-				}
-				this.bus.$emit('dataTypes', {datasource: ft, datatype: st});
-			}
-		}
-	}
+    export default {
+        data() {
+            return {
+                eniqkpi: "ENIQ-TDD",
+                nbmkpi: "NBM",
+                kgetkpi: "KGET",
+                mrkpi: "MR",
+                ctrkpi: "CTR",
+                ctumkpi: "CTUM",
+                ebmkpi: "EBM"
+            }
+        },
+        watch: {
+        },
+        methods: {
+            handleCommand(command) {
+                var ft = command.split('-')[0], 
+                    st = command.split('-')[1];
+                this.eniqkpi = 'ENIQ';
+                this.nbmkpi = 'NBM';
+                this.kgetkpi = 'KGET';
+                this.mrkpi = "MR";
+                this.ctrkpi = "CTR";
+                this.ctumkpi = "CTUM";
+                this.ebmkpi = "EBM";
+                switch (ft)
+                {
+                    case 'ENIQ':
+                        this.eniqkpi = command;
+                        break;
+                    case 'NBM':
+                        this.nbmkpi = command;
+                        break;
+                    case 'KGET':
+                        this.kgetkpi = command;
+                        break;
+                    case 'MR':
+                        this.mrkpi = command;
+                        break;
+                    case 'CTR':
+                        this.ctrkpi = command;
+                        break;
+                    case 'CTUM':
+                        this.ctumkpi = command;
+                        break;
+                    case 'EBM':
+                        this.ebmkpi = command;
+                        break;
+                    default:
+                        this.eniqkpi = command;
+                        break;
+                }
+                this.bus.$emit('dataTypes', {datasource: ft, datatype: st})
+            }
+        }
+    }
 </script>
