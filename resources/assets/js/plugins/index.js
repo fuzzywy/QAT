d340823495666cf622032bd20f3d165e557e93d6
1nv0ker <760604256@qq.com>
2019-04-01 15:39:20 +0800

    优化下目录结构 | tdd查询显示10条数据改成显示1000条 | nbm查询显示1000条数据
    Change-Id: I072b972daea2e2d089df086d327c6c98fb4a9977

---
+++ resources/assets/js/plugins/index.js
@@ -0,0 +1,9 @@
+import './axios'
+import './ElementUI'
+import './qs'
+// import './BootstrapVue'//这样导入bootstrap-vue无法识别
+import './VueHighcharts'
+import './Highcharts'
+import './bus'
+import './loadsh'
+import './jquery'
