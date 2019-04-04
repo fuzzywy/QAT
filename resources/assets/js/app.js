d181addddbe571601e111bbb8c0e90e2384e53d9
1nv0ker <760604256@qq.com>
2019-04-04 09:16:01 +0800

    NBM和ENQI显示限制为1000条,导出限制为一百万条 | 优化下插件的目录结构
    Change-Id: I12d5954d7050900fd8a1b2842d10dcaf11cb579b

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,28 +1,12 @@
-window._ = require('lodash');
-try {
-    window.$ = window.jQuery = require('jquery');
-} catch (e) {}
-window.axios = require('axios');
-window.qs = require('qs');
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-let token = document.head.querySelector('meta[name="csrf-token"]');
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
+//Vue
 window.Vue = require('vue');
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(ElementUI, { size: 'big', zIndex: 3000 });
+//插件
+import './plugins';
+//路由
 import router from './routes.js';
+//状态管理 
 import store from './store.js';
-window.BootstrapVue  = require('bootstrap-vue');
-window.VueHighcharts = require('vue-highcharts');
-window.Highcharts = require('highcharts');
-//data transform
-const bus = new Vue()
-Vue.prototype.bus = bus
+
 const app = new Vue({
   router,
   store
